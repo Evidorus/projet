2342746a2327c0ad93dc12de823ba528431d6ce0
Saurav Barua <75080137+sauravBarua@users.noreply.github.com>
2021-02-16 10:34:18 +0100

    Update home-jsx

--- trippy/src/view/Home.jsx
+++ trippy/src/view/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 class Home extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = { 
+    cities: [] 
+    };
+    componentWillUnmount() {
+      getHomeData()
+  }
+}
   render() {
     return (
       <>
