71ab7c0caeb0ab208eb5e52328d896cc04c3fca2
Ewa Kadziolka <kalistaewa@gmail.com>
2021-02-16 15:52:49 +0100

    home with cityCards

--- trippy/src/components/CityCard.jsx
+++ trippy/src/components/CityCard.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
- class CityCard extends React.Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class CityCard extends React.Component {
+  render() {
+    return (
+      <Link to={`/hotel/${this.props.cityName}`} className="col-4">
+        <img
+          className="col-11"
+          src={`http://localhost:3002${this.props.cityImage}`}
+          alt={this.props.cityName}
+        />
+        <h3 className="text-center">{this.props.cityName}</h3>
+      </Link>
+    );
+  }
 }
 export default CityCard;
--- trippy/src/view/Home.jsx
+++ trippy/src/view/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import getHomeData from "../utils/Api";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CityCard from "../components/CityCard";
 
 class Home extends React.Component {
   constructor(props) {
@@ -25,12 +27,21 @@ class Home extends React.Component {
       return <h2>Les villes sont en train de charger</h2>;
     } else {
       return (
-        <>
-          <h1>Découvrir le monde </h1>
-          {this.state.cities.map((city) => {
-            return <p>{city.name}</p>;
-          })}
-        </>
+        <BrowserRouter>
+          <div className="container">
+            <h1>Découvrir le monde </h1>
+            <div className="row ">
+              {this.state.cities.map((city) => {
+                return (
+                  <CityCard
+                    cityName={city.name}
+                    cityImage={city.source}
+                  ></CityCard>
+                );
+              })}
+            </div>
+          </div>
+        </BrowserRouter>
       );
     }
   }
