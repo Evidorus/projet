f0cb36a34540460cc963460a0392284b14719b3e
Ewa <kalistaewa@gmail.com>
2021-02-18 12:43:11 +0100

    map in HotelPage donne

--- trippy/src/view/HotelPage.jsx
+++ trippy/src/view/HotelPage.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import maison from "../components/maison.png";
+
 import "../App.css";
 
 class HotelPage extends React.Component {
@@ -26,13 +30,42 @@ class HotelPage extends React.Component {
   };
 
   render() {
+    var greenIcon = L.icon({
+      iconUrl: maison,
+      iconSize: [38, 45], // size of the icon
+      shadowSize: [50, 64], // size of the shadow
+      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62], // the same for the shadow
+      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    });
+
     if (this.state.hotel.length === 0) {
       return <h2>Ca charge baka, soit patient !!</h2>;
     } else {
       return (
-        <>
-          <h1>{this.state.hotel.name}Bonjour je suis chargé</h1>
-        </>
+        <div className="container">
+          <h1 className="mt-3">{this.state.hotel.name}</h1>
+          <h2>{this.state.hotel.price}€</h2>
+          <Map
+            style={{ height: 500, width: 500, objectFit: "cover" }}
+            center={[
+              this.state.hotel.location.lat,
+              this.state.hotel.location.lon,
+            ]}
+            zoom={12}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker icon={greenIcon} position={this.state.hotel.location}>
+              <Popup>
+                <div>{this.state.hotel.price}€</div>
+              </Popup>
+            </Marker>
+          </Map>
+        </div>
       );
     }
   }
