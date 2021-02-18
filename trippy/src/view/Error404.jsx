2db0f719f497930947307c3e7188e005b7774461
Ewa <kalistaewa@gmail.com>
2021-02-18 20:01:16 +0100

    error404

--- trippy/src/view/Error404.jsx
+++ trippy/src/view/Error404.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "../App.css"
-
+import "../App.css";
 
 class Error404 extends React.Component {
   render() {
     return (
-      <>
-        <h1 className="caption">Voici la page Error 404 </h1>
-      </>
+      <div className="container">
+        <h1 className="text-center m-5">Cette page n'existe pas.</h1>
+        <div className="d-flex justify-content-center">
+          <img src="/7iJR.gif" alt="error 404" />
+        </div>
+      </div>
     );
   }
 }
